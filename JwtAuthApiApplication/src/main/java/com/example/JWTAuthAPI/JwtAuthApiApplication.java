bfdb4dfd8a3c1bde243069e84bc99b27c5300c62
LukasDD82 <lukas.simek.de@gmail.com>
2026-08-12 09:59:48 +0200

    test: configure H2 dialect and disable OSIV for integration tests

--- JwtAuthApiApplication/src/main/java/com/example/JWTAuthAPI/JwtAuthApiApplication.java
+++ JwtAuthApiApplication/src/main/java/com/example/JWTAuthAPI/JwtAuthApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtAuthAPIApplication {
+public class JwtAuthApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JwtAuthAPIApplication.class, args);
+		SpringApplication.run(JwtAuthApiApplication.class, args);
 	}
 
 }
